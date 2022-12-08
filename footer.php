8026883ddeb73faa5334ea14c18a77386591b6f6
KuzeyKurt <japarov.s.i20@gmail.com>
2022-12-08 17:05:12 +0300

    start theme for wordpress

---
+++ footer.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package qasaba
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<p><?=date('Y') ?> | All rights reserved. </p>
+		</div><!-- .site-info -->
+
+<div>
+	<nav class="footer-navigation">
+<button class="menu-toggle" aria-controls="footer-menu" aria-expanded="false"><?php esc_html_e( 'Footer Menu', 'qasaba' ); ?></button>
+	<?php wp_nav_menu( 
+		array(
+			'theme_location' => 'footer-menu',
+			'menu_id'        => 'footer-menu',
+		)
+	) ?>
+	</nav>
+</div>
+
+		<div class="logo">         
+   <?php the_custom_logo(); ?>     
+  </div> <!-- custom logo -->
+
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ inc/customizer.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * qasaba Theme Customizer
+ *
+ * @package qasaba
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function qasaba_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+	// попытка сделать кастомный хедер 
+	$wp_customize->get_setting( 'header_color')->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'qasaba_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'qasaba_customize_partial_blogdescription',
+			)
+		);
+
+		$wp_customize->selective_refresh->add_partial(
+			'headercolor',
+			array(
+				'selecor' => '.'
+			)
+		)
+	}
+// добавление контроля цветов
+	$wp_customize -> add_setting('qasaba_link_color', array(
+		'default' => 'ffffff',
+		'transport' => 'refresh',
+	));
+
+	$wp_customize -> add_section('qasaba_standard_colors', array(
+		'title' => __('Standard Colors', 'qasaba'),
+		'priority' => 30,
+	));
+
+	$wp_customize -> add_control( new WP_Customize_Color_Control( $wp_customize, 'qasaba_link_color_control', array(
+		'label' => __('Link Color', 'qasaba'),
+		'section' => 'qasaba_link_color',
+		'settings' =>  'lwp_link_color'
+	)));
+}
+add_action( 'customize_register', 'qasaba_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function qasaba_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function qasaba_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function qasaba_customize_preview_js() {
+	wp_enqueue_script( 'qasaba-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+}
+add_action( 'customize_preview_init', 'qasaba_customize_preview_js' );
